5019f3ebf977d0633cf0691e8dc6aa97df30a72e
GraemeW <graeme.m.williams@gmail.com>
2014-11-09 22:55:37 -0500

    Updated homepage for external site linkage
    Added neat CSS overlay on external site links

--- classes/gHome.class.php
+++ classes/gHome.class.php
@@ -14,16 +14,34 @@ class gHome extends gPage {
 		<div id="home-container">
 		<?php
 
-		$total_query = $page_db->Select('SELECT gcode, title, sdesc, gimg FROM gpage WHERE 1');
+		$total_query = $page_db->Select('SELECT gcode, title, external, sdesc, gimg FROM gpage WHERE 1');
 		shuffle($total_query);
 
 		for ($i=0; $i < count($total_query); $i++)
 		{
 		?>
 			<div id="home-split">
-				<a href="index.php?id=<? echo $total_query[$i]['gcode']; ?>">
-				<img src="S1-img/collage/<? echo $total_query[$i]['gimg']; ?>" alt="<? echo $total_query[$i]["gcode"]; ?>" title="<? echo $total_query[$i]["title"]; ?>">
-				</a>
+				<?
+                if($total_query[$i]['external']==1)
+                {
+                ?>
+                <div class="overtop">
+                <a href="index.php?id=<? echo $total_query[$i]['gcode']; ?>">
+                <img src="S1-img/collage/<? echo $total_query[$i]['gimg']; ?>" alt="<? echo $total_query[$i]["gcode"]; ?>" title="<? echo $total_query[$i]["title"]; ?>">
+                <h2><span>External <br /> Website</span></h2>
+                </a>
+                </div>
+                <?
+                }
+                else 
+                {
+                ?>
+                <a href="index.php?id=<? echo $total_query[$i]['gcode']; ?>">
+                <img src="S1-img/collage/<? echo $total_query[$i]['gimg']; ?>" alt="<? echo $total_query[$i]["gcode"]; ?>" title="<? echo $total_query[$i]["title"]; ?>">
+                </a>
+                <?    
+                }
+                ?>
 			</div>
 		<? } ?>
 		
